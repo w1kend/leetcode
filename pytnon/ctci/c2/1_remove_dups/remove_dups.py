0c09def61906f14d8fa86502ed90ab090dd63197
Sergei Makarov <wikendp@gmail.com>
2023-10-05 00:59:18 +0200

    [python] ctci 2.1 remove duplicates in a linked list

---
+++ pytnon/ctci/c2/1_remove_dups/remove_dups.py
@@ -0,0 +1,32 @@
+import unittest
+
+import LinkedList
+
+
+# Write code to remove duplicates from an unsorted linked list
+def remove_deps_hashmap(root: LinkedList.LinkedList) -> LinkedList.LinkedList:
+    values = dict()
+
+    curr = root.head
+    values[curr.value] = True
+
+    while curr.next:
+        if curr.next.value in values:
+            curr.next = curr.next.next
+        else:
+            values[curr.next.value] = True
+            curr = curr.next
+
+    return root
+
+
+class Test(unittest.TestCase):
+    def test_remove_deps(self):
+        tests = [
+            (LinkedList.LinkedList([1, 1, 2, 2, 2, 3, 4, 5]), LinkedList.LinkedList([1, 2, 3, 4, 5])),
+            (LinkedList.LinkedList([1, 1, 1]), LinkedList.LinkedList([1])),
+            (LinkedList.LinkedList([1]), LinkedList.LinkedList([1])),
+        ]
+
+        for [l, expect] in tests:
+            self.assertEqual(str(expect), str(remove_deps_hashmap(l)))
