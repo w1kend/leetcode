eafbd754241845b45cfc627acc7ba447492a2f39
Sergei Makarov <wikendp@gmail.com>
2023-10-13 23:45:14 +0200

    [python] ctci 3.5 sort stack

---
+++ pytnon/ctci/c3/sort_stack.py
@@ -0,0 +1,51 @@
+import unittest
+
+from pytnon.ctci.c3.stack import Stack
+
+
+# Write a program to sort a stack such that the smallest items are on the top. You can use an additional temporary
+# stack, but you may not copy the elements into any other data structure (such as an array). The stack supports the
+# following operations: push, pop, peek, and isEmpty
+def sort_stack(main: Stack) -> Stack:
+    tmp = Stack()
+    source_length = len(main)
+    n = 0  # number of correctly placed elements
+    while n < source_length - 1:
+        if not tmp:
+            tmp.put(main.pop())
+        # swap them. moving the largest element to the bottom of the stack
+        if main.peek() <= tmp.peek():
+            tmp_val = tmp.pop()
+            tmp.put(main.pop())
+            main.put(tmp_val)
+            if len(main) == n + 1:
+                n += 1
+            else:
+                tmp.put(main.pop())
+        elif len(main) > n:
+            tmp.put(main.pop())
+
+        if len(main) == n:
+            # place all the elements back to the original stack and do again
+            while tmp:
+                main.put(tmp.pop())
+
+    while tmp:
+        main.put(tmp.pop())
+
+    return main
+
+
+class Test(unittest.TestCase):
+    def test_sort_stack(self):
+        tests = [
+            (Stack([1, 4, 2, 3]), Stack([4, 3, 2, 1])),
+            (Stack([1, 2, 3, 4, 5, 6]), Stack([6, 5, 4, 3, 2, 1])),
+            (Stack([1, 6, 6, 5, 3, 1, 2]), Stack([6, 6, 5, 3, 2, 1, 1])),
+            (Stack([1, 4, 2]), Stack([4, 2, 1])),
+            (Stack([1, 2]), Stack([2, 1])),
+            (Stack([2]), Stack([2])),
+        ]
+
+        for [test, expect] in tests:
+            self.assertEqual(str(expect), str(sort_stack(test)))
--- pytnon/ctci/c3/stack.py
+++ pytnon/ctci/c3/stack.py
@@ -1,7 +1,10 @@
 class Stack():
 
-    def __init__(self):
+    def __init__(self, values=None):
         self.buf = []
+        if values:
+            for val in values:
+                self.put(val)
 
     def put(self, value):
         self.buf.append(value)
@@ -19,3 +22,6 @@ class Stack():
 
     def __bool__(self):
         return bool(self.buf)
+
+    def __str__(self):
+        return f'{self.buf}'
