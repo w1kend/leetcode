6a65281ba3dd2d15306472072e39d7672d37e910
Sergei Makarov <wikendp@gmail.com>
2023-10-23 23:40:25 +0200

    [python] ctci 4.7 build order with DFS

--- pytnon/ctci/с4/7_build_order/build_order.py
+++ pytnon/ctci/с4/7_build_order/build_order.py
@@ -1,5 +1,21 @@
 import unittest
-from typing import List
+from typing import List, Optional
+
+
+class Node:
+    def __init__(self, name: str):
+        self.children: list[Node] = []
+        self.name = name
+
+
+class Graph:
+    def __init__(self, nodes: list[Node], adjacency: List[tuple[str, str]]):
+        self.nodes: dict[str, Node] = {node.name: node for node in nodes}
+        self.__build(adjacency)
+
+    def __build(self, adjacency: List[tuple[str, str]]):
+        for start, finish in adjacency:
+            self.nodes[start].children.append(self.nodes[finish])
 
 
 def build_order(projects: List[str], dependencies: List[tuple[str, str]]) -> List[str]:
@@ -25,6 +41,39 @@ def build_order(projects: List[str], dependencies: List[tuple[str, str]]) -> Lis
     return build_sequence
 
 
+def build_order_dfs(projects: List[str], dependencies: List[tuple[str, str]]) -> List[str]:
+    # build a graph
+    nodes: List[Node] = [Node(project) for project in projects]
+    adjacency_list: List[tuple[str, str]] = [(dependency, project) for dependency, project in dependencies]
+    graph: Graph = Graph(nodes, adjacency_list)
+
+    created_projects: set[str] = set()
+    creation_sequence: List[str] = []
+
+    for project, node in graph.nodes.items():
+        if project not in created_projects:
+            dfs(node, created_projects, creation_sequence)
+
+    creation_sequence.reverse()
+    return creation_sequence
+
+
+def dfs(node: Optional[Node], created: set[str], sequence: List[str]):
+    # create project if it doesn't have any dependencies
+    if len(node.children) == 0:
+        created.add(node.name)
+        sequence.append(node.name)
+        return
+
+    for child in node.children:
+        # create all dependencies first
+        if child.name not in created:
+            dfs(child, created, sequence)
+    # create current project
+    created.add(node.name)
+    sequence.append(node.name)
+
+
 class Test(unittest.TestCase):
     def test_build_order(self):
         tests = [
@@ -37,7 +86,8 @@ class Test(unittest.TestCase):
                     ('f', 'a'),
                     ('d', 'c'),
                 ],
-                ['e', 'f', 'a', 'b', 'd', 'c']
+                ['e', 'f', 'a', 'b', 'd', 'c'],
+                ['f', 'e', 'b', 'a', 'd', 'c']
             ),
             (
                 ["a", "b", "c", "d", "e", "f", "g"],
@@ -52,8 +102,10 @@ class Test(unittest.TestCase):
                     ("f", "b"),
                 ],
                 ['d', 'f', 'g', 'b', 'c', 'a', 'e'],
+                ['f', 'd', 'g', 'c', 'b', 'a', 'e'],
             )
         ]
 
-        for projects, dependencies, expected in tests:
+        for projects, dependencies, expected, graph_expected in tests:
             self.assertEqual(expected, build_order(projects, dependencies))
+            self.assertEqual(graph_expected, build_order_dfs(projects, dependencies))
