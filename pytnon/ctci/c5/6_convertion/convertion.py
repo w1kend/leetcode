0a30107bd59a04b96d37b9af560d1a0f5c9e41c1
Sergei Makarov <wikendp@gmail.com>
2023-10-26 16:43:47 +0200

    [python] ctci 5.6 convertion

---
+++ pytnon/ctci/c5/6_convertion/convertion.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+def flips_to_convert(a: int, b: int) -> int:
+    diff = a ^ b
+
+    cnt = 0
+    while diff != 0:
+        if diff & 1 == 1:
+            cnt += 1
+        diff >>= 1
+    return cnt
+
+
+class Test(unittest.TestCase):
+    def test_flips_co_convert(self):
+        tests = [
+            (int('11101', 2), int('01111', 2), 2),
+            (int('101', 2), int('1010', 2), 4),
+            (int('101', 2), int('1011', 2), 3),
+            (int('101', 2), int('101', 2), 0),
+        ]
+
+        for a, b, expected in tests:
+            self.assertEqual(expected, flips_to_convert(a, b))
